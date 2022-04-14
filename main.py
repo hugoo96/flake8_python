ae2bd081a04388a634f03944ffbef803c751dbbc
Hugo Albuquerque <hugoo_96@hotmail.com>
2022-04-14 19:22:06 -0300

    teste com importação e quebra de linha

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 from fila_prioritaria import FilaPrioritaria
 
-# fila = FilaPrioritaria()
-#
-# fila.atualiza_fila()
-# fila.atualiza_fila()
-#
-# print(fila.chama_cliente(10))
-# print(fila.chama_cliente(5))
-#
-# print(fila.estatistica('14/04/2022', 1898, 'detail'))
-print()
+fila = FilaPrioritaria()
+
+fila.atualiza_fila()
+fila.atualiza_fila()
+
+print(fila.chama_cliente(10))
+print(fila.chama_cliente(5))
+
+print(fila.estatistica('14/04/2022', 197, 'detail'))
