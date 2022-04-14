ec7c57e397a1f82f3e6530dca4021b66f05932f1
Hugo Albuquerque <hugoo_96@hotmail.com>
2022-04-14 19:58:46 -0300

    corrigindo erros flake8

---
+++ fila_normal.py
@@ -0,0 +1,17 @@
+from fila_base import FilaBase
+
+
+class FilaNormal(FilaBase):
+
+    def gera_senha_atual(self) -> None:
+        self.senha_atual = f'NM{self.codigo}'
+
+    def atualiza_fila(self):
+        self.reseta_fila()
+        self.gera_senha_atual()
+        self.fila.append(self.senha_atual)
+
+    def chama_cliente(self, caixa: int) -> str:
+        cliente_atual = self.fila.pop(0)
+        self.clientes_atendidos.append(cliente_atual)
+        return f'Cliente atual: {cliente_atual} dirija-se ao caixa {caixa}'
--- fila_prioritaria.py
+++ fila_prioritaria.py
@@ -1,31 +1,22 @@
+from fila_base import FilaBase
 
 
-class FilaPrioritaria:
-    codigo: int = 0
-    fila = []
-    clientes_atendidos = []
-    senha_atual: str = ''
+class FilaPrioritaria(FilaBase):
 
-    def gera_senha_atual(self)-> None:
+    def gera_senha_atual(self) -> None:
         self.senha_atual = f'PR{self.codigo}'
-
-    def reseta_fila(self)-> None:
-        if self.codigo >= 100:
-            self.codigo = 0
-        else:
-            self.codigo += 1
 
     def atualiza_fila(self):
         self.reseta_fila()
         self.gera_senha_atual()
         self.fila.append(self.senha_atual)
 
-    def chama_cliente(self, caixa:int)-> str:
+    def chama_cliente(self, caixa: int) -> str:
         cliente_atual = self.fila.pop(0)
         self.clientes_atendidos.append(cliente_atual)
         return f'Cliente atual: {cliente_atual} dirija-se ao caixa {caixa}'
 
-    def estatistica(self, dia: str, agencia: int, flag: str)-> dict:
+    def estatistica(self, dia: str, agencia: int, flag: str) -> dict:
         if flag != 'detail':
             estatistica = {f'{agencia}-{dia}': len(self.clientes_atendidos)}
         else:
@@ -33,6 +24,7 @@ class FilaPrioritaria:
             estatistica['dia'] = dia
             estatistica['agencia'] = agencia
             estatistica['clientes_atendidos'] = self.clientes_atendidos
-            estatistica['quantidade_clientes_atendidos'] = len(self.clientes_atendidos)
+            estatistica[
+                'quantidade_clientes_atendidos'] = len(self.clientes_atendidos)
 
         return estatistica
