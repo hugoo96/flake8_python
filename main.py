795d02ac0fab21e55232c43260096e9de1f3e3a9
Hugo Albuquerque <hugoo_96@hotmail.com>
2022-04-14 19:00:18 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+from fila_prioritaria import FilaPrioritaria
+
+# fila = FilaPrioritaria()
+#
+# fila.atualiza_fila()
+# fila.atualiza_fila()
+#
+# print(fila.chama_cliente(10))
+# print(fila.chama_cliente(5))
+#
+# print(fila.estatistica('14/04/2022', 1898, 'detail'))
+print()
