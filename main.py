ec91074510de8eef10dd1753bbbfe923f4c613f4
Hugo Albuquerque <hugoo_96@hotmail.com>
2022-04-17 06:57:23 -0300

    criando injeção de dependencia

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from fabrica_fila import FabricaFila
+from estatistica_resumida import EstatisticaResumida
+from estatistica_detalhada import EstatisticaDetalhada
 
 fila = FabricaFila.pega_fila('prioritaria')
 
@@ -8,4 +10,5 @@ fila.atualiza_fila()
 print(fila.chama_cliente(10))
 print(fila.chama_cliente(5))
 
-print(fila.estatistica('15/04/2022', 1898, 'detail'))
+print(fila.estatistica(EstatisticaDetalhada("17/04/2022", 1898)))
+print(type(fila.estatistica))
